f620a9b2e0e4c5f48053147c87dc800c8212fb2c
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2024-12-31 21:25:45 +0100

    impl: feat: more shapes

--- implementation/src/editor/block/shape/Shapes.ts
+++ implementation/src/editor/block/shape/Shapes.ts
@@ -43,8 +43,9 @@ export const shapes = [
         name: "egg",
         label: "Egg",
         html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-  <path d="M50 5C30 5 20 35 20 55C20 75 35 95 50 95C65 95 80 75 80 55C80 35 70 5 50 5Z" fill="var(--color)" />
-</svg>`,
+  <path d="M50 0C30 0 20 30 20 50C20 70 35 100 50 100C65 100 80 70 80 50C80 30 70 0 50 0Z" fill="var(--color)" />
+</svg>
+`,
         nonProportionalResizing: false
     },
     {
@@ -54,7 +55,58 @@ export const shapes = [
   <polygon points="50,0 61,33 100,33 67,61 78,100 50,75 22,100 33,61 0,33 39,33" fill="var(--color)" />
 </svg>`,
         nonProportionalResizing: true
+    },
+    {
+        name: "heart",
+        label: "Heart",
+        html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="5 5 90 90" preserveAspectRatio="none">
+              <path d="M50 91S5 55 5 30C5 10 30 0 50 30C70 0 95 10 95 30C95 55 50 91 50 91Z" fill="var(--color)" />
+           </svg>`,
+        nonProportionalResizing: true
+    },
+    {
+        name: "pentagon",
+        label: "Pentagon",
+        html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+              <polygon points="50,0 100,38 81,100 19,100 0,38" fill="var(--color)" />
+           </svg>`,
+        nonProportionalResizing: true
+    },
+    {
+        name: "hexagon",
+        label: "Hexagon",
+        html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+              <polygon points="50,0 100,25 100,75 50,100 0,75 0,25" fill="var(--color)" />
+           </svg>`,
+        nonProportionalResizing: true
+    },
+    {
+        name: "crescent",
+        label: "Crescent",
+        html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+              <path d="M100 50A50 50 0 1 1 50 0A40 40 0 1 0 100 50Z" fill="var(--color)" />
+           </svg>`,
+        nonProportionalResizing: true
+    },
+    {
+        name: "ladder",
+        label: "Ladder",
+        class: ["one-line", "grow-2"],
+        html: `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 100" preserveAspectRatio="none">
+  <rect x="0" y="0" width="10" height="100" fill="var(--color)" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+  <rect x="0" y="20" width="100" height="20" fill="var(--color)" />
+  <rect x="0" y="60" width="100" height="20" fill="var(--color)" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 100" preserveAspectRatio="none">
+  <rect x="0" y="0" width="10" height="100" fill="var(--color)" />
+</svg>
+`,
+        nonProportionalResizing: true
     }
+
 ] as {
     name: string;
     label: string;
