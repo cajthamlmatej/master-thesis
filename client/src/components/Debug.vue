31db0f9daf4ea0f603fef53dc5694f8bda5eba36
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-01-02 17:11:24 +0100

    impl: feat: better color readability, transfer of debug to variables

--- client/src/components/Debug.vue
+++ client/src/components/Debug.vue
@@ -10,12 +10,18 @@ const version = `v${import.meta.env.VITE_APP_VERSION}`;
 <style scoped lang="scss">
 .debug {
     position: fixed;
-    bottom: -2px;
+    bottom: 0;
     right: 0;
-    background-color: white;
-    border-radius: 5px;
-    margin: 10px;
+
+    margin: 0.5em;
+    padding: 0.25em 0.5em;
+
+    border-radius: 0.25em;
+
     font-size: 0.8em;
     font-weight: bold;
+
+    background-color: var(--color-debug-background);
+    color: var(--color-debug-text);
 }
 </style>
