e3789aed4e104a7456a2b0147c430244e0c78f50
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-01-05 23:01:17 +0100

    impl: feat: docs: initialize

---
+++ docs/docs/intro.md
@@ -0,0 +1,29 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+Welcome to the **MASTERTHESIS** documentation!
+
+This documentation is intended to provide a comprehensive guide to the **MASTERTHESIS** project. It is divided into
+several sections, each of which covers a different aspect of the project. The sections are as follows:
+
+- [Introduction](./intro.md): An overview of the project, its goals, objectives and the origins of the project.
+- [Installation](./installation.md): Instructions on how to install the project on your local machine.
+- [Project Structure](./project-structure.md): An overview of the project's structure and the various components that make up the project.
+  - [Client](./client.md): An overview of the client-side code and how it interacts with the server.
+  - [Server](./server.md): An overview of the server-side code and how it interacts with the client.
+  - [Documentation](./documentation.md): About the documentation of the project.
+  - [Homepage](./homepage.md): About the homepage of the project.
+  - [Platform](./platform.md): About the platform of the project which is used to host the project.
+- [Usage](./usage.md): Instructions on how to use the project and its various features.
+- [Contributing](./contributing.md): Information on how to contribute to the project and the guidelines for contributing.
+- [License](./license.md): Information on the license under which the project is released.
+- [Acknowledgements](./acknowledgements.md): Acknowledgements to the people and organizations that have contributed to the project.
+- [Contact](./contact.md): Contact information for the project maintainers.
+- [FAQ](./faq.md): Frequently asked questions about the project.
+- [Changelog](./changelog.md): A log of changes made to the project over time.
+- [Roadmap](./roadmap.md): The roadmap for the project and the features that are planned for future releases.
+
+To extend the application, you can start by reading the [How to extend the application](./extend.md) section.
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,94 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Masterthesis Documentation',
+  tagline: 'Not every presentation is material, but yours is definitely worth presenting!',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Masterthesis',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://masterthesis.cajthaml.dev/',
+          label: 'Website',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Bc. Matěj Cajthaml`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
