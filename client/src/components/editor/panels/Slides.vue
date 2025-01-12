1569b320be6f7e616fda952e16da8e460a4b15b3
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-01-12 19:31:04 +0100

    impl: feat: client: blocks panel redesign

--- client/src/components/editor/panels/Slides.vue
+++ client/src/components/editor/panels/Slides.vue
@@ -1,10 +1,11 @@
 <template>
     <Navigation v-model:menu="slides" shift full-control>
         <template #primary>
-            <div class="menu" ref="menu">
+            <div class="menu editor-slides" ref="menu">
                 <div class="actions">
-                    <Button icon="plus" @click="materialStore.newSlide"/>
-                    <Button icon="minus" @click="removeActiveSlide" :disabled="canRemoveSlide"/>
+                    <Button icon="plus" fluid @click="materialStore.newSlide">
+                        Add slide
+                    </Button>
                 </div>
 
                 <div class="slides">
@@ -20,6 +21,7 @@
                             <div class="actions">
                                 <i class="mdi mdi-arrow-up" @click="materialStore.moveSlide(slide, -1)" :class="{disabled: i === 0}"/>
                                 <i class="mdi mdi-arrow-down" @click="materialStore.moveSlide(slide, 1)" :class="{disabled: i === materialStore.getSlides().length - 1}"/>
+                                <i class="mdi mdi-trash-can-outline" @click="removeSlide(slide)" :class="{disabled: canRemoveSlide}"/>
                             </div>
                         </div>
                     </div>
@@ -33,6 +35,7 @@
 
 import {computed, onMounted, onUnmounted, ref, watch} from "vue";
 import {useMaterialStore} from "@/stores/material";
+import type Slide from "@/models/Slide";
 
 const slides = ref(true);
 
@@ -71,102 +74,7 @@ const removeActiveSlide = () => {
         materialStore.removeSlide(active);
     }
 }
+const removeSlide = (slide: Slide) => {
+    materialStore.removeSlide(slide);
+}
 </script>
-
-<style scoped lang="scss">
-.menu {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-
-    > .actions {
-        position: sticky;
-        top: 0;
-        z-index: 1;
-        padding: 0.5rem;
-        border-bottom: var(--nagivation-border-width) solid var(--color-navigation-border);
-        background-color: var(--color-background);
-
-        display: flex;
-        justify-content: center;
-        gap: 0.25rem;
-        flex-shrink: 0;
-    }
-}
-
-
-.slides {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    gap: 1rem;
-    padding: 0.5rem;
-    overflow-y: auto;
-    flex-grow: 1;
-
-    .slide {
-        padding: 0.25em;
-        border-radius: 0.5rem;
-        cursor: pointer;
-        background-color: var(--color-background-accent);
-        color: var(--color-text);
-
-        width: 100%;
-
-        &.slide--active {
-            outline: 2px solid var(--color-primary);
-        }
-
-        > .image {
-            width: 100%;
-            height: 100%;
-            aspect-ratio: 16/9;
-            background-color: var(--color-background);
-
-            background-size: contain;
-            background-position: center;
-            background-repeat: no-repeat;
-
-            &:not([src]) {
-                //opacity: 0;
-            }
-        }
-
-        > .slide-meta {
-            color: var(--color-text);
-            font-size: 0.75rem;
-            padding: 0.25rem 0.25rem 0;
-
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-
-            > .actions {
-                display: flex;
-                gap: 0.25rem;
-
-                .mdi {
-                    cursor: pointer;
-                    background: var(--color-background-accent);
-                    padding: 0.1rem;
-                    border-radius: 0.25rem;
-
-                    &:hover {
-                        background: var(--color-background);
-                    }
-
-                    &.disabled {
-                        cursor: not-allowed;
-                        background: var(--color-background-accent);
-
-                        &:hover {
-                            background: var(--color-background-accent);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-</style>
