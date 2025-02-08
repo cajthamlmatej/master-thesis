bf182ef056d9da3f4e27897d30ad61b28b8660b8
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-02-08 16:12:19 +0100

    impl: feat: client: changing language component

---
+++ client/src/components/ChangeLanguage.vue
@@ -0,0 +1,52 @@
+<template>
+    <Dialog>
+        <template #activator="{ toggle }">
+            <NavigationButton icon="translate-variant" @click="toggle" :label="language"></NavigationButton>
+        </template>
+        <template #default>
+            <Card dialog>
+                <p class="title">Change language</p>
+
+                <List>
+                    <ListItem v-for="language in translation.getLanguages()"
+                              hover
+                              :active="translation.getLanguage() == language.code"
+                              @click="changeLanguage(language.code)">
+                        {{ language.label }}
+                    </ListItem>
+                </List>
+            </Card>
+        </template>
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import NavigationButton from "@/components/design/navigation/NavigationButton.vue";
+import {ref} from "vue";
+import {translation} from "@/translation/Translation";
+import {useRoute, useRouter} from "vue-router";
+
+const language = ref(translation.getLanguage());
+
+translation.LANGUAGE_CHANGED.on(() => {
+    language.value = translation.getLanguage();
+});
+
+const router = useRouter();
+const route = useRoute();
+const changeLanguage = (code: string) => {
+    router.push({
+        name: route.name as string,
+        params: {
+            ...route.params ?? {},
+            lang: code
+        }
+    });
+
+    translation.changeLanguage(code);
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
