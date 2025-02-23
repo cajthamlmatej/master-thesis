b9d70775d908812122144baf70d31d47b56cc3a8
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-02-23 14:24:40 +0100

    impl: feat: client: plugin models and mapper

---
+++ client/src/models/Plugin.ts
@@ -0,0 +1,31 @@
+import moment from "moment";
+
+export class PluginRelease {
+    version: string;
+    date: moment.Moment;
+    changelog: string;
+    manifest: string;
+    editorCode: string;
+    pluginCode: string;
+}
+
+export default class Plugin {
+    author: string;
+    id: string;
+    name: string;
+    icon: string;
+    description: number;
+    tags: string[];
+    releases: PluginRelease[];
+
+    constructor(author: string, id: string, name: string, icon: string, description: number, tags: string[], releases: PluginRelease[]) {
+        this.author = author;
+        this.id = id;
+        this.name = name;
+        this.icon = icon;
+        this.description = description;
+        this.tags = tags;
+        this.releases = releases;
+    }
+
+}
---
+++ client/src/models/mappers/PluginMapper.ts
@@ -0,0 +1,26 @@
+import {PluginDTO} from "../../../lib/dto/plugin/PluginDTO";
+import Plugin from "@/models/Plugin";
+import moment from "moment";
+
+export default class PluginMapper {
+
+    public static fromPluginDTO(dto: PluginDTO | Omit<PluginDTO, "releases">): Plugin {
+        return new Plugin(
+            dto.author,
+            dto.id,
+            dto.name,
+            dto.icon,
+            dto.description,
+            dto.tags,
+            ("releases" in dto ? dto.releases : []).map((r) => ({
+                version: r.version,
+                date: moment(r.date),
+                changelog: r.changelog,
+                manifest: r.manifest,
+                editorCode: r.editorCode,
+                pluginCode: r.pluginCode
+            }))
+        )
+    }
+
+}
