cf8dee0f270661997c08763ca2036de81d9304ef
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-04-06 20:11:51 +0200

    impl: feat: server: plugins, user plugins, creating release

--- server/src/plugin/plugin.controller.ts
+++ server/src/plugin/plugin.controller.ts
@@ -1,15 +1,17 @@
-import {Controller, Get, Param, Req} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Req, UnauthorizedException, UseGuards} from '@nestjs/common';
 import {PluginService} from "./plugin.service";
 import {RequestWithUser} from "../types";
 import {AllPluginsSuccessDTO} from "../../dto/plugin/AllPluginsSuccessDTO";
 import {SpecificPluginsSuccessDTO} from "../../dto/plugin/SpecificPluginsSuccessDTO";
+import {RequiresAuthenticationGuard} from "../auth/auth.guard";
+import {CreatePluginReleaseDTO} from "../../dto/plugin/CreatePluginReleaseDTO";
 
-@Controller('plugin')
+@Controller('')
 export class PluginController {
     constructor(private readonly pluginService: PluginService) {
     }
 
-    @Get('/')
+    @Get('/plugin')
     public async getPlugins(@Req() req: RequestWithUser) {
         const plugins = await this.pluginService.getPlugins();
 
@@ -27,7 +29,37 @@ export class PluginController {
         } as AllPluginsSuccessDTO;
     }
 
-    @Get('/:ids')
+
+    @Get("/user/:user/plugin")
+    @UseGuards(RequiresAuthenticationGuard)
+    public async findAllForUser(@Param('user') user: string, @Req() req: RequestWithUser) {
+        const authenticatedUser = req.user;
+
+        if (req.user.id !== user) throw new UnauthorizedException('You are not allowed to access this resource');
+
+        const plugins = await this.pluginService.findAllForUser(authenticatedUser);
+
+        return {
+            plugins: plugins.map((p) => ({
+                id: p.id,
+                author: p.author ? p.author.name : "Unknown",
+                name: p.name,
+                icon: p.icon,
+                description: p.description,
+                tags: p.tags,
+                releases: p.releases.map((r) => ({
+                    version: r.version,
+                    date: r.date.toISOString(),
+                    changelog: r.changelog,
+                    manifest: r.manifest,
+                    editorCode: r.editorCode,
+                    playerCode: r.playerCode
+                }))
+            }))
+        } as AllPluginsSuccessDTO;
+    }
+
+    @Get('/plugin/:ids')
     public async getPlugin(@Param('ids') ids: string) {
         const pluginIds = ids.split(",");
 
@@ -51,5 +83,25 @@ export class PluginController {
                 }))
             }))
         } as SpecificPluginsSuccessDTO;
+    }
+
+    @Post('/plugin/:id/release')
+    @UseGuards(RequiresAuthenticationGuard)
+    public async createPluginRelease(@Param('id') id: string, @Body() body: CreatePluginReleaseDTO, @Req() req: RequestWithUser) {
+        const plugin = await this.pluginService.getPluginById(id);
+
+        if (!plugin) {
+            throw new UnauthorizedException('Plugin not found');
+        }
+
+        if (plugin.author.toString() !== req.user.id.toString()) {
+            throw new UnauthorizedException('You are not allowed to access this resource');
+        }
+
+        await this.pluginService.createPluginRelease(plugin, body);
+
+        return {
+            success: true
+        }
     }
 }
--- server/src/plugin/plugin.service.ts
+++ server/src/plugin/plugin.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
-import {Model} from "mongoose";
+import {HydratedDocument, Model} from "mongoose";
 import {Plugin} from "./plugin.schema";
+import {User} from "../users/user.schema";
+import {CreatePluginReleaseDTO} from "../../dto/plugin/CreatePluginReleaseDTO";
 
 @Injectable()
 export class PluginService {
@@ -18,5 +20,28 @@ export class PluginService {
                 $in: pluginIds
             }
         }).exec();
+    }
+
+    async findAllForUser(authenticatedUser: HydratedDocument<User>) {
+        return this.pluginModel.find({
+            author: authenticatedUser._id
+        });
+    }
+
+    async getPluginById(id: string) {
+        return this.pluginModel.findById(id).exec();
+    }
+
+    async createPluginRelease(plugin: HydratedDocument<Plugin>, body: CreatePluginReleaseDTO) {
+        plugin.releases.push({
+            version: body.version,
+            date: new Date(),
+            changelog: body.changelog,
+            manifest: body.manifest,
+            editorCode: body.editorCode,
+            playerCode: body.playerCode
+        });
+
+        await plugin.save();
     }
 }
