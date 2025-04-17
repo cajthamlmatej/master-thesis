a73c27935c33fdd3c2e84c208f943ed7ff9b7403
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-04-17 19:06:21 +0200

    home: fix: useless script

--- homepage/src/views/HomeView.vue
+++ homepage/src/views/HomeView.vue
@@ -145,9 +145,6 @@
 </template>
 
 <script setup lang="ts">
-</script>
-
-<script setup lang="ts">
 import {useScrollReact} from "@/composables/scrollReact.ts";
 import {computed, onMounted, ref, watch} from "vue";
 import Slider from "@/components/Slider.vue";
