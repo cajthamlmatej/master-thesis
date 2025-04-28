1652ed615f375e5a67490113535b64c2e54ba35c
Matěj DenOwq Cajthaml <cajthaml.matej@navarru.cz>
2025-04-28 09:21:05 +0200

    impl: fix: server: generate thumbnails limit bump

--- server/src/materials/materialsExport.service.ts
+++ server/src/materials/materialsExport.service.ts
@@ -68,7 +68,7 @@ export class MaterialsExportService {
                 await page.goto(
                     `${url}/${material.id}?slide=${slideId}&rendering=true&cookies=true&token=${this.getToken()}`, {waitUntil: 'networkidle2'});
 
-                await page.waitForSelector(".editor-content", {timeout: 10000});
+                await page.waitForSelector(".editor-content", {timeout: 30000});
                 await page.waitForFunction(() => {
                     const element = document.querySelector("[data-cy=cookies]");
                     return !element;
