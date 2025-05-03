3d38fb656202672de0ab2463f2e76b121f41afce
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2025-05-03 11:33:19 +0200

    server: feat: changing password + notification on email about that

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -14,11 +14,15 @@ import {RequestWithUser} from "../types";
 import {UsersService} from "./users.service";
 import OneUserSuccessDTO from "../../dto/user/OneUserSuccessDTO";
 import UpdateUserDTO from "../../dto/user/UpdateUserDTO";
+import { EmailService } from 'src/email/email.service';
+import { EmailTemplates } from 'src/email/email.template';
 
 @Controller('user')
 export class UsersController {
     constructor(
-        private readonly usersService: UsersService) {
+        private readonly usersService: UsersService,
+        private readonly emailService: EmailService,
+    ) {
     }
 
     @Get("/:id")
@@ -54,9 +58,20 @@ export class UsersController {
 
         if (!target) throw new NotFoundException('User does not exist');
 
-        const validPassword = await this.usersService.comparePassword(target, update.currentPassword);
+        if(update.name) {
+            if(!update.currentPassword) throw new UnauthorizedException('You need to provide your current password to update your name');
 
-        if (!validPassword) throw new UnauthorizedException('Invalid password');
+            const validPassword = await this.usersService.comparePassword(target, update.currentPassword);
+
+            if (!validPassword) throw new UnauthorizedException('Invalid password');
+        }
+
+        if(update.password) {
+            this.emailService.sendEmail(target.email, EmailTemplates.PASSWORD_CHANGED, {
+                name: target.name,
+            });
+        }
+
 
         await this.usersService.updateUser(id, {
             password: update.password ? (await this.usersService.hashPassword(update.password)) : undefined,
